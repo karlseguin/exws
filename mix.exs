243671f13c58b9b44fdce01299baf17a061a4859
Karl Seguin <k@openmymind.io>
2022-11-02 15:53:46 +0800

    add ex_doc dependency for publishing to hex.pm

--- mix.exs
+++ mix.exs
@@ -5,12 +5,20 @@ defmodule ExWs.Mixfile do
 		[
 			app: :exws,
 			deps: deps(),
-			version: "0.0.1",
+			version: "0.0.2",
 			elixir: "~> 1.14",
 			elixirc_paths: paths(Mix.env),
 			build_embedded: Mix.env == :prod,
 			start_permanent: Mix.env == :prod,
-			compilers: Mix.compilers
+			compilers: Mix.compilers,
+			description: "Elixir Websocket Server - Kitchen Sink Not Included",
+			package: [
+				licenses: ["MIT"],
+				links: %{
+					"git" => "https://github.com/karlseguin/exws"
+				},
+				maintainers: ["Karl Seguin"]
+			]
 		]
 	end
 
@@ -26,6 +34,7 @@ defmodule ExWs.Mixfile do
 
 	defp deps do
 		[
+			{:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
 			{:jiffy, git: "https://github.com/karlseguin/jiffy", only: [:test, :ab]}
 		]
 	end
